88502c44f3ed4264693a679bdf535567b9c2540f
Franq <kuj-fra@wp.pl>
2023-07-20 19:27:57 +0200

    image is zoomable

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,29 @@ sf::Sprite PrepareToDraw(const Image& image)
     return shape;
 }
 
-void Run(sf::RenderWindow& window, const sf::Sprite& shape)
+void ViewControl(sf::View& view, sf::Event& event)
+{
+    static sf::Clock timer;
+    sf::Time elapsedTime = timer.getElapsedTime();
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Add) && elapsedTime.asSeconds() > 0.1)
+    {
+        view.zoom(0.98);
+        timer.restart();
+        return;
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract) && elapsedTime.asSeconds() > 0.1)
+    {
+        view.zoom(1.02);
+        timer.restart();
+        return;
+    }
+    if(event.type == sf::Event::MouseWheelMoved)
+    {
+        view.zoom((-1 * event.mouseWheel.delta) * 0.02 + 1);
+    }
+}
+
+void Run(sf::RenderWindow& window, const sf::Sprite& shape, sf::View& view)
 {
     while (window.isOpen())
     {
@@ -46,7 +68,10 @@ void Run(sf::RenderWindow& window, const sf::Sprite& shape)
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            ViewControl(view, event);
         }
+
+        window.setView(view);
 
         window.clear();
         window.draw(shape);
@@ -59,20 +84,15 @@ int main(int argc, char* argv[])
     const auto [screenWidth, screenHeight] = GetScreenSize();
 
     sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML works!");
+    sf::View view(sf::FloatRect(0.f, 0.f, screenWidth, screenHeight));
+    window.setView(view);
 
     Image image(argc > 1 ? argv[1] : "..//images//zdj1.jpg");
     image.Transform();
 
     sf::Sprite shape = PrepareToDraw(image);
-    /*
-    sf::Texture texture;
-    texture.loadFromImage(image.GetImage());
-
-    shape.setPosition(0, 0);
-    shape.setTexture(texture); // raczej błąd
-    */
     FitImageToScreen(image, shape, screenWidth, screenHeight);
-    Run(window, shape);
+    Run(window, shape, view);
 
     return 0;
 }
